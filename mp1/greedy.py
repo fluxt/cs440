4a2235467449a24992230f0e3294a5e1c096e73e
Henry <henry@laptop.localdomain>
2018-01-30 19:43:43 -0600

    sanitied inputs and added greedy.py

---
+++ mp1/greedy.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+import fileinput
+
+grid = np.array([[c for c in line if (c != '\n' and c != '\r')] for line in fileinput.input()]).T
+
+width, height = grid.shape
+
+print(width)
+print(height)
+
+graph = {(x, y): [] for x in range(width) for y in range(height) if grid[x][y] != '%'}
+
+for (x, y) in graph:
+  if (x+1) < width and grid[x+1, y] != '%':
+    graph[(x, y)].append((x+1, y)) #right
+  if (x-1) >= 0 and grid[x-1, y] != '%':
+    graph[(x, y)].append((x-1, y)) #left
+  if (y+1) < height and grid[x, y+1] != '%':
+    graph[(x, y)].append((x, y+1)) #down
+  if (y-1) >= 0 and grid[x, y-1] != '%':
+    graph[(x, y)].append((x, y-1)) # up
+
+print(graph[(2, 1)])
+
