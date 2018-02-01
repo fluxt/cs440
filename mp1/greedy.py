04d34bf74e92edc5bc86d6178f5fa2b5b6fea734
Henry Choi <googolydox@gmail.com>
2018-01-31 20:02:59 -0600

    haewpifohpaiwehfa

--- mp1/greedy.py
+++ mp1/greedy.py
@@ -5,7 +5,7 @@ import utils
 def heuristics(pos1, pos2):
 	return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[0])
 
-def gbfs(grid, graph, pacman_position, goal_positions):
+def gbfs(graph, pacman_position, goal_positions):
 	goal = goal_positions[0]
 	prev = {}
 	visited = {pos: False for pos in graph}
@@ -25,22 +25,21 @@ def gbfs(grid, graph, pacman_position, goal_positions):
 	path = []
 	current = goal
 	while current in prev:
-		current = prev[current]
 		path.append(current)
-
-	for pos in path[:-1]:
-		grid[pos[0]][pos[1]] = '.'
+		current = prev[current]
+	path.append(pacman_position)
 
 	# print(list(visited.values()).count(True))
 	# print(path)
 	# print(len(path))
 
-	return grid, path
+	path.reverse()
+	return path
 
 if __name__ == "__main__":
 	grid, graph, pacman_position, goal_positions = utils.load_puzzle("part1/mediumMaze.txt")
 	
-	grid_output, path = gbfs(grid, graph, pacman_position, goal_positions)
-	
-	utils.print_grid(grid_output)
-	utils.print_grid_to_file(grid_output, "greedy_output.txt")
+	path = gbfs(graph, pacman_position, goal_positions)
+
+	utils.draw_solution_to_grid(grid, path)
+	utils.print_grid(grid)
