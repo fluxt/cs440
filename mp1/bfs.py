d1a1303f83888d4e0a25de32d4a5fabea953b30e
Michelle Lamblin <lamblin2@siebl-0403-33.ews.illinois.edu>
2018-02-07 19:52:50 -0600

    added comments to bfs and dfs

--- mp1/bfs.py
+++ mp1/bfs.py
@@ -4,39 +4,48 @@ from collections import defaultdict
 
 
 def bfs(graph, pacman_pos, goal_position):
+	# dictionary of nodes visited
 	distance = {node:None for node in graph}
-	#distance dict
 	distance[pacman_pos] = 0
-    # predecessors dict
+	# dictionary of node's parents
 	parent = defaultdict(lambda: None)
+	# push start position into the queue (list)
 	queue = [pacman_pos]
 	nodes_expanded = 0
 	while queue:
+		 	# pop the first element from the queue
 			coordinate = queue.pop(0)
 			nodes_expanded += 1
+			# if the current coordinate is equivalent to the end position exit the while loop and return shortest path
 			if coordinate == goal_position:
 				break
+			# loop through all of the neighbors within the current coordinate in the graph
 			for vertex in graph[coordinate]:
 				if distance[vertex] is None:
 						distance[vertex] = distance[coordinate] + 1
+						# increase the distance of the vertex by the coordinate+1
 						queue.append(vertex)
+						# update the list of parent nodes in the dictionary
 						parent[vertex] = coordinate
 
-    # retrieve shortest path
 	path = []
 	n = goal_position
+	# loop through the grid from the goal position and update the shortest path
 	while n is not None:
+    		# insert the shortest path into the return path
 			path.insert(0,n)
+			# set the next node to the parent of the previous inserted node
 			n = parent[n]
 	return path, nodes_expanded
 
 if __name__ == "__main__":
 	files = ["part1/mediumMaze.txt", "part1/bigMaze.txt", "part1/openMaze.txt"]
-	
+
 	for file in files:
+		# pull the required files and information for bfs from utils.py
 		grid, graph, pacman_position, goal_positions = utils.load_puzzle(file)
 		path, nodes_expanded = bfs(graph, pacman_position, goal_positions[0])
-
+		# print out graph solution, path cost, and number of expanded nodes
 		utils.draw_solution_to_grid(grid, path)
 		utils.print_grid(grid)
 		print("path cost is  : ", len(path)-1)
--- mp1/dfs.py
+++ mp1/dfs.py
@@ -3,40 +3,48 @@ import utils
 from collections import defaultdict
 
 def dfs(graph, pacman_pos, goal_position):
+	# dictionary of nodes visited
 	distance = {node:None for node in graph}
-	#distance dict
 	distance[pacman_pos] = 0
-    # predecessors dict
+    # dictionary of node's parents
 	parent = defaultdict(lambda: None)
+	# push start position into the stack (list)
 	stack = [pacman_pos]
 	nodes_expanded = 0
-	#iterate through the stack and check each object in the graph
 	while stack:
+			# pop the last element from the stack
 			coordinate = stack.pop()
 			nodes_expanded += 1
+			# if the current coordinate is equivalent to the end position exit the while loop and return shortest path
 			if coordinate == goal_position:
 				break
+	     	# loop through all of the neighbors within the current coordinate in the graph
 			for vertex in graph[coordinate]:
 				if distance[vertex] is None:
 						distance[vertex] = distance[coordinate] + 1
+						# increase the distance of the vertex by the coordinate+1
 						stack.append(vertex)
+						# update the list of parent nodes in the dictionary
 						parent[vertex] = coordinate
 
-    # retrieve shortest path
 	path = []
 	n = goal_position
+	# loop through the grid from the goal position and update the shortest path
 	while n is not None:
+		    # insert the shortest path into the return path
 			path.insert(0,n)
+			# set the next node to the parent of the previous inserted node
 			n = parent[n]
 	return path, nodes_expanded
 
 if __name__ == "__main__":
 	files = ["part1/mediumMaze.txt", "part1/bigMaze.txt", "part1/openMaze.txt"]
-	
+
 	for file in files:
+		# pull the required files and information for dfs from utils.py
 		grid, graph, pacman_position, goal_positions = utils.load_puzzle(file)
 		path, nodes_expanded = dfs(graph, pacman_position, goal_positions[0])
-
+		# print out graph solution, path cost, and number of expanded nodes
 		utils.draw_solution_to_grid(grid, path)
 		utils.print_grid(grid)
 		print("path cost is  : ", len(path)-1)
