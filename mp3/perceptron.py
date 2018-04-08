984f3953059085c83850452336fae55978a3600b
HoelzelJon <hoelzeljon@gmail.com>
2018-04-08 17:45:37 -0500

    added some comments, cleaned up the code, and changed the output graphs a bit

--- mp3/perceptron.py
+++ mp3/perceptron.py
@@ -4,22 +4,31 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import utils
-import math
 import random
 
 num_digits = 10
 img_width = 32
 img_height = 32
+init_weights_sd_dev = 10
 
+img_folder = "images/"
+
+num_epochs = 25
+num_repeats = 20
+
+def learning_rate_function(epoch_num):
+    return 1 / (epoch_num + 1)
+
+# adds a 1 to the end of each image in the list
 def add_bias(images):
     return np.concatenate((images, np.array([[1] for n in range(images.shape[0])])), axis=1)
 
-#sizes is the number of neurons in respective layers of network
 class PerceptionLearningRule:
     def __init__(self, train_images, train_numbers, b, rand_init_weights, rand_order):
         if np.size(train_images, 0) != np.size(train_numbers, 0):
             print("Error: image size does not match numbers size")
             return
+
         self.bias = b
         self.random_init_weights = rand_init_weights
         self.random_order = rand_order
@@ -28,12 +37,7 @@ class PerceptionLearningRule:
             self.img_size += 1
 
         self.num_images = train_images.shape[0];
-        #layers: number of neurons per layer in network
-        #self.num_layers = len(sizes)
-        #total size of neurons in layers of network
-        #self.sizes = sizes
 
-        #biases: initialized randomly utilizing a gaussian distribution
         if (bias):
             self.train_images = add_bias(train_images)
         else:
@@ -41,18 +45,23 @@ class PerceptionLearningRule:
 
         self.train_numbers = train_numbers
 
-        #weights: initialized randomly using gaussian dist. mean 0/var 1
         if random_init_weights:
-            self.weights = np.array([[random.random() - .5 for x  in range(self.img_size)] for y in range(num_digits)])
+            # use a random distribution to initialize the weights
+            self.weights = np.array([[np.random.randn() * init_weights_sd_dev for x  in range(self.img_size)] for y in range(num_digits)])
         else:
             self.weights = np.zeros((num_digits, self.img_size))
 
+    # returns an array containing the value for each class (digits 0-9)
     def get_output(self, image):
         return np.array([np.dot(image, self.weights[i]) for i in range(num_digits)])
 
+    # returns the perceptron's best guess for the provided image
     def get_guess(self, image):
         return np.argmax(self.get_output(image))
 
+    # returns a tuple containing:
+    #   - the accuracy of the perceptron over the set of images provided
+    #   - the confusion matrix c, where c[i][j] is the proportion of the items in class i that were classified into class j by the perceptron
     def evaluate(self, test_images, test_numbers):
         if (bias):
             test_images = add_bias(test_images)
@@ -70,7 +79,8 @@ class PerceptionLearningRule:
             confusion_matrix[i] /= np.sum(confusion_matrix[i])
         return (num_correct / len(test_numbers)), confusion_matrix
 
-    def do_epoch(self, learning_rate = 0.001):
+    # does one epoch of learning, with the specified learning rate
+    def do_epoch(self, learning_rate):
         if self.random_order:
             order = [i for i in range(self.num_images)]
             random.shuffle(order)
@@ -83,29 +93,42 @@ class PerceptionLearningRule:
             guess = self.get_guess(image)
             real = self.train_numbers[i]
 
+            # adjust the weights if the perceptron guessed wrong
             if (guess != real):
                 self.weights[guess] -= image * learning_rate
                 self.weights[real] += image * learning_rate
 
 if __name__ == "__main__":
+    # seed the random number generators
+    random.seed(10)
+    np.random.seed(10)
 
     train_images, train_numbers = utils.get_train_data()
     test_images, test_numbers = utils.get_test_data()
 
-    for i in range(8):
-        bias = (i & 4) == 0
-        random_init_weights = (i & 2) == 0
-        random_order = (i & 1) == 0
+    # replace the zeros in the data with -1
+    train_images[train_images == 0] = -1
+    test_images[test_images == 0] = -1
 
-        random.seed(10)
-        classifier = PerceptionLearningRule(train_images, train_numbers, bias, random_init_weights, random_order)
+    # stores the accuracies of each setup
+    accuracies = np.zeros((8, num_epochs))
+    # stores the labels each setup for the plot
+    names = []
 
-        accuracies = []
-        for i in range(23):
-            accuracy, _ = classifier.evaluate(train_images, train_numbers)
-            accuracies.append(accuracy)
+    for setup_num in range(8):
+        bias = (setup_num & 4) == 0
+        random_init_weights = (setup_num & 2) == 0
+        random_order = (setup_num & 1) == 0
 
-            classifier.do_epoch(learning_rate = 1 / ((i+1) ** 0.5))
+        # run num_repeat trials for each setup
+        for repeat_num in range(num_repeats):
+            classifier = PerceptionLearningRule(train_images, train_numbers, bias, random_init_weights, random_order)
+
+            for epoch_num in range(num_epochs):
+                accuracy, _ = classifier.evaluate(train_images, train_numbers)
+                accuracies[setup_num][epoch_num] += accuracy
+
+                classifier.do_epoch(learning_rate_function(epoch_num))
 
         name = ""
         if (bias):
@@ -114,34 +137,53 @@ if __name__ == "__main__":
             name += "No Bias, "
 
         if (random_init_weights):
-            name += "Rnd. Init Weights, "
+            name += "Rnd. Init Wts, "
         else:
-            name += "Weights Init to 0, "
+            name += "Wts Init to 0, "
 
         if (random_order):
-            name += "Rnd. Training Order"
+            name += "Rnd. Order"
         else:
-            name += "Set Training Order"
-        plt.plot(accuracies, label = name)
+            name += "Set Order"
+        names.append(name)
+        print("Finished with: " + name)
 
+    accuracies /= num_repeats
+
+    # plot the accuracies of each of the setups
+    for i in range(8):
+        plt.plot(accuracies[i], label = names[i])
     plt.title("Perceptron Accuracy by Epoch")
     plt.xlabel("Epoch Number")
     plt.ylabel("Accuracy (Training Data)")
     plt.legend()
     plt.ylim((0, 1))
-    plt.savefig("perceptron_accuracies.png")
-    plt.title("Perceptron Accuracy by Epoch (Zoom)")
-    plt.ylim((0.9, 1))
-    plt.savefig("perceptron_accuracies_zoom.png")
+    plt.xlim((0, num_epochs - 1))
+    plt.savefig(img_folder + "perceptron_accuracies.png")
 
+    plt.clf()
+
+    # plot the accuracies of each of te setups on a log scale
+    for i in range(8):
+        plt.plot(1 - accuracies[i], label = names[i])
+    plt.title("Perceptron Accuracy by Epoch (Log scale)")
+    plt.xlabel("Epoch Number")
+    plt.ylabel("1 - Accuracy (Training Data)")
+    plt.legend()
+    plt.yscale('log', nonposy='clip')
+    plt.xlim((0, num_epochs - 1))
+    plt.savefig(img_folder + "perceptron_accuracies_log.png")
+
+    # Run the perceptron once more to get the images of the weights, as well as the final accuracy and confusion matrix
     classifier = PerceptionLearningRule(train_images, train_numbers, False, True, True)
+    for epoch_num in range(num_epochs):
+        classifier.do_epoch(learning_rate_function(epoch_num))
 
-    for i in range(23):
-        classifier.do_epoch()
-
+    # create each of the
     for i in range(num_digits):
-        filename = "weights_" + str(i) + ".png"
-        utils.write_image_to_file_colored(filename, classifier.weights[i])
+        utils.write_image_to_file_colored(img_folder + "weights_" + str(i) + ".png", classifier.weights[i])
 
-    _, confusion = classifier.evaluate(test_images, test_numbers)
-    #print(np.around(confusion, 3))
+    # get the accuracy and confusion matrix, based on the test images
+    accuracy, confusion = classifier.evaluate(test_images, test_numbers)
+    print("Accuracy: " + str(accuracy))
+    print(np.around(confusion, 3))
