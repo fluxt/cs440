4812213f4b2af81ff42106a5a7aa8def31164950
HoelzelJon <hoelzeljon@gmail.com>
2018-04-09 10:48:35 -0500

    changed some variable names and the order of some things

--- mp3/perceptron.py
+++ mp3/perceptron.py
@@ -16,16 +16,16 @@ img_folder = "images/"
 num_epochs = 25
 num_repeats = 20
 
-def learning_rate_function(epoch_num):
-    return 1 / (epoch_num + 1)
+def learning_rate_function(time):
+    return 1 / (time + 1)
 
 # adds a 1 to the end of each image in the list
 def add_bias(images):
     return np.concatenate((images, np.array([[1] for n in range(images.shape[0])])), axis=1)
 
 class PerceptionLearningRule:
-    def __init__(self, train_images, train_numbers, b, rand_init_weights, rand_order):
-        if np.size(train_images, 0) != np.size(train_numbers, 0):
+    def __init__(self, training_images, training_numbers, b, rand_init_weights, rand_order):
+        if np.size(training_images, 0) != np.size(training_numbers, 0):
             print("Error: image size does not match numbers size")
             return
 
@@ -33,51 +33,47 @@ class PerceptionLearningRule:
         self.random_init_weights = rand_init_weights
         self.random_order = rand_order
         self.img_size = img_width * img_height
-        if (bias):
+        if (b):
             self.img_size += 1
 
-        self.num_images = train_images.shape[0];
+        self.num_images = training_images.shape[0];
 
-        if (bias):
-            self.train_images = add_bias(train_images)
+        if (b):
+            self.train_images = add_bias(training_images)
         else:
-            self.train_images = train_images
+            self.train_images = training_images
 
-        self.train_numbers = train_numbers
+        self.train_numbers = training_numbers
 
-        if random_init_weights:
+        if rand_init_weights:
             # use a random distribution to initialize the weights
             self.weights = np.array([[np.random.randn() * init_weights_sd_dev for x  in range(self.img_size)] for y in range(num_digits)])
         else:
             self.weights = np.zeros((num_digits, self.img_size))
 
-    # returns an array containing the value for each class (digits 0-9)
-    def get_output(self, image):
-        return np.array([np.dot(image, self.weights[i]) for i in range(num_digits)])
-
     # returns the perceptron's best guess for the provided image
-    def get_guess(self, image):
-        return np.argmax(self.get_output(image))
+    def get_guess(self, guess_image):
+        return np.argmax(np.array([np.dot(guess_image, self.weights[i]) for i in range(num_digits)]))
 
     # returns a tuple containing:
     #   - the accuracy of the perceptron over the set of images provided
     #   - the confusion matrix c, where c[i][j] is the proportion of the items in class i that were classified into class j by the perceptron
-    def evaluate(self, test_images, test_numbers):
-        if (bias):
-            test_images = add_bias(test_images)
+    def evaluate(self, eval_images, eval_numbers):
+        if (self.bias):
+            eval_images = add_bias(eval_images)
 
         num_correct = 0
         confusion_matrix = np.zeros((num_digits, num_digits))
 
-        for i in range(len(test_numbers)):
-            guess = self.get_guess(test_images[i])
-            if (guess == test_numbers[i]):
+        for i in range(len(eval_numbers)):
+            guess = self.get_guess(eval_images[i])
+            if (guess == eval_numbers[i]):
                 num_correct += 1
-            confusion_matrix[test_numbers[i]][guess] += 1
+            confusion_matrix[eval_numbers[i]][guess] += 1
 
         for i in range(num_digits):
             confusion_matrix[i] /= np.sum(confusion_matrix[i])
-        return (num_correct / len(test_numbers)), confusion_matrix
+        return (num_correct / len(eval_numbers)), confusion_matrix
 
     # does one epoch of learning, with the specified learning rate
     def do_epoch(self, learning_rate):
@@ -109,6 +105,22 @@ if __name__ == "__main__":
     # replace the zeros in the data with -1
     train_images[train_images == 0] = -1
     test_images[test_images == 0] = -1
+
+    # Run the perceptron to get the images of the weights, as well as the final accuracy and confusion matrix
+    classifier = PerceptionLearningRule(train_images, train_numbers, True, False, False)
+    for epoch_num in range(num_epochs):
+        classifier.do_epoch(learning_rate_function(epoch_num))
+
+    # create each of the images of the weight vectors
+    for i in range(num_digits):
+        utils.write_image_to_file_colored(img_folder + "weights_" + str(i) + ".png", classifier.weights[i][:(img_height * img_width)])
+
+    # get the accuracy and confusion matrix, based on the test images
+    accuracy, confusion = classifier.evaluate(test_images, test_numbers)
+    print("Accuracy: " + str(accuracy))
+    print(np.around(confusion, 3))
+
+    # create the learning curves
 
     # stores the accuracies of each setup
     accuracies = np.zeros((8, num_epochs))
@@ -146,7 +158,7 @@ if __name__ == "__main__":
         else:
             name += "Set Order"
         names.append(name)
-        print("Finished with: " + name)
+        #print("Finished with: " + name)
 
     accuracies /= num_repeats
 
@@ -173,17 +185,3 @@ if __name__ == "__main__":
     plt.yscale('log', nonposy='clip')
     plt.xlim((0, num_epochs - 1))
     plt.savefig(img_folder + "perceptron_accuracies_log.png")
-
-    # Run the perceptron once more to get the images of the weights, as well as the final accuracy and confusion matrix
-    classifier = PerceptionLearningRule(train_images, train_numbers, False, True, True)
-    for epoch_num in range(num_epochs):
-        classifier.do_epoch(learning_rate_function(epoch_num))
-
-    # create each of the
-    for i in range(num_digits):
-        utils.write_image_to_file_colored(img_folder + "weights_" + str(i) + ".png", classifier.weights[i])
-
-    # get the accuracy and confusion matrix, based on the test images
-    accuracy, confusion = classifier.evaluate(test_images, test_numbers)
-    print("Accuracy: " + str(accuracy))
-    print(np.around(confusion, 3))
