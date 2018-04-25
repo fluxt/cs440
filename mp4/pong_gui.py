6b23f244ffd6dba6ea65ce10c03d903a1fe9abba
Henry Choi <googolydox@gmail.com>
2018-04-24 21:52:16 -0500

    working gui integrated with pong game

--- mp4/pong_gui.py
+++ mp4/pong_gui.py
@@ -1,4 +1,7 @@
 import sys, pygame
+import game
+import random
+
 pygame.init()
 
 size = width, height = [750, 720]
@@ -15,14 +18,21 @@ ballspeed = [1, 1]
 paddlepos = 0
 paddlespeed = 1
 
-while 1:
+g = game.Game()
+
+# while True:
+while not g.lost_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # ballpos = [720 * ball_x, 720 * ball_y]
-    # paddlepos = [720 * paddle_y]
+    ballpos = [round(720 * g.ball_x), round(720 * g.ball_y)]
+    ballspeed = [round(720 * g.velocity_x), round(720 * g.velocity_y)]
+    paddlepos = round(720 * g.paddle_y)
+
+
+
 
     ballpos = [x+y for x,y in zip(ballpos, ballspeed)]
     paddlepos = paddlepos + paddlespeed
@@ -34,6 +44,12 @@ while 1:
         paddlespeed = - paddlespeed
 
     screen.fill(white)
-    pygame.draw.circle(screen, red, (ballpos[0], ballpos[1]), 10)
     pygame.draw.rect(screen, black, (720,paddlepos,30,144))
+    pygame.draw.circle(screen, red, (ballpos[0], ballpos[1]), 10)
+    pygame.draw.line(screen, blue, (ballpos[0], ballpos[1]), (ballpos[0]+ballspeed[0], ballpos[1]+ballspeed[1]))
+
     pygame.display.flip()
+
+    g.do_frame(random.choice([game.Action.UP, game.Action.NOTHING, game.Action.DOWN]))
+
+    pygame.time.delay(50)
