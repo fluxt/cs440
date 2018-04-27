c2ff0aa53dabc4eb1a7a18674670f7a50da37924
Henry Choi <googolydox@gmail.com>
2018-04-26 23:21:54 -0500

    human

---
+++ mp4/game_human.py
@@ -0,0 +1,95 @@
+from enum import Enum
+import random
+import numpy as np
+
+class Action(Enum):
+    DOWN = 0.04
+    NOTHING = 0.0
+    UP = -0.04
+
+paddle_height = 0.2
+discrete_end_game_state = (-1, -1, -1, -1, -1)
+
+class GameHuman:
+    def __init__(self):
+        self.ball_x = 0.5
+        self.ball_y = 0.5
+        self.velocity_x = 0.03
+        self.velocity_y = 0.01
+        self.paddle_y = 0.5 - (paddle_height / 2)
+        self.human_y = 0.5 - (paddle_height / 2)
+
+        self.current_reward = 0
+        self.bounces = 0
+
+    def lost_game(self):
+        return (self.ball_x > 1 or self.ball_x < 0)
+
+    # returns the reward for the action taken (-1, 0, or 1)
+    def do_frame(self, act, human):
+        if self.lost_game():
+            self.current_reward = -1
+            return
+
+        self.paddle_y += act.value
+        self.human_y += human.value
+
+        if (self.paddle_y < 0):
+            self.paddle_y = 0
+        elif (self.paddle_y + paddle_height > 1):
+            self.paddle_y = 1 - paddle_height
+
+        if (self.human_y < 0):
+            self.human_y = 0
+        elif (self.human_y + paddle_height > 1):
+            self.human_y = 1 - paddle_height
+
+        self.ball_x += self.velocity_x
+        self.ball_y += self.velocity_y
+
+        if (self.ball_y < 0):
+            self.ball_y = -self.ball_y
+            self.velocity_y = - self.velocity_y
+        elif (self.ball_y > 1):
+            self.ball_y = 2 - self.ball_y
+            self.velocity_y = - self.velocity_y
+
+        if (self.ball_x < 0 and self.human_y < self.ball_y < self.human_y + paddle_height):
+            self.ball_x = - self.ball_x
+            self.velocity_x = - self.velocity_x
+
+            if (abs(self.velocity_x) < 0.03):
+                self.velocity_x = 0.03 if self.velocity_x > 0 else -0.03
+
+            self.velocity_y = self.velocity_y + random.uniform(-.03, .03)
+            return
+
+        elif (self.ball_x > 1 and self.paddle_y < self.ball_y < self.paddle_y + paddle_height):
+            self.ball_x = 2 - self.ball_x
+            self.velocity_x = - self.velocity_x + random.uniform(-.015, .015)
+
+            if (abs(self.velocity_x) < 0.03):
+                self.velocity_x = 0.03 if self.velocity_x > 0 else -0.03
+
+            self.velocity_y = self.velocity_y + random.uniform(-.03, .03)
+            self.current_reward = 1
+            self.bounces += 1
+            return
+        self.current_reward = 0
+
+    def get_state(self):
+        return np.array([self.ball_x, self.ball_y, self.velocity_x, self.velocity_y, self.paddle_y])
+
+    def get_discrete_state(self):
+        return discrete_end_game_state if self.lost_game() else (int(self.ball_x * 12), int(self.ball_y * 12), -1 if self.velocity_x < 0 else 1, -1 if self.velocity_y < -0.015 else (1 if self.velocity_y > 0.015 else 0), 11 if self.paddle_y == 1 - paddle_height else int(12 * self.paddle_y / (1 - paddle_height)))
+
+    def get_current_reward(self):
+        return self.current_reward
+
+    def get_num_bounces(self):
+        return self.bounces
+
+if __name__ == "__main__":
+    g = GameHuman()
+    while not g.lost_game():
+        g.do_frame(Action.NOTHING, Action.NOTHING)
--- mp4/pong_gui_human.py
+++ mp4/pong_gui_human.py
@@ -1,8 +1,9 @@
-import sys, pygame
+import sys
+import pygame
 import game
+import game_human
 import random
-
-pygame.init()
+import q_learning
 
 size = width, height = [780, 720]
 black = [0, 0, 0]
@@ -11,48 +12,59 @@ red = [255, 0, 0]
 green = [0, 255, 0]
 blue = [0, 0, 255]
 
-screen = pygame.display.set_mode(size)
+class PongGUIHuman():
+    def init(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(size)
 
-ballpos = [0,0]
-ballspeed = [1, 1]
-paddlepos = 0
-paddlespeed = 1
+    def refresh(self, g, delay):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-def refresh(g, delay):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        ballpos = [30+round(719 * g.ball_x), round(719 * g.ball_y)]
+        ballspeed = [round(719 * g.velocity_x), round(719 * g.velocity_y)]
+        paddlepos = round(719 * g.paddle_y)
+        humanpos = round(719 * g.human_y)
 
-    # keys = pygame.key.get_pressed()
-    # action = game.Action.NOTHING
-    # if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-    #     action = game.Action.UP
-    # elif not keys[pygame.K_UP] and keys[pygame.K_DOWN]:
-    #     action = game.Action.DOWN
+        self.screen.fill(white)
+        pygame.draw.rect(self.screen, black, (0,humanpos,30,720*0.2))
+        pygame.draw.rect(self.screen, black, (750,paddlepos,30,720*0.2))
+        pygame.draw.circle(self.screen, red, (ballpos[0], ballpos[1]), 10)
+        pygame.draw.line(self.screen, blue, (ballpos[0], ballpos[1]), (ballpos[0]+ballspeed[0], ballpos[1]+ballspeed[1]))
 
-    # g.do_frame(action)
-
-    # print(g.get_state())
-
-    ballpos = [30+round(719 * g.ball_x), round(719 * g.ball_y)]
-    ballspeed = [round(719 * g.velocity_x), round(719 * g.velocity_y)]
-    paddlepos = round(719 * g.paddle_y)
-
-    screen.fill(white)
-    pygame.draw.rect(screen, black, (0,paddlepos,30,720*0.2))
-    pygame.draw.rect(screen, black, (750,paddlepos,30,720*0.2))
-    pygame.draw.circle(screen, red, (ballpos[0], ballpos[1]), 10)
-    pygame.draw.line(screen, blue, (ballpos[0], ballpos[1]), (ballpos[0]+ballspeed[0], ballpos[1]+ballspeed[1]))
-
-    pygame.display.flip()
-    pygame.time.delay(delay)
+        pygame.display.flip()
+        pygame.time.delay(delay)
 
 if __name__ == "__main__":
+    exploration_count = 20
+    C = 20
+    gamma = 0.95
+    training_games = 30000
+
+    print("training...")
+    agent = q_learning.Q_Learner(gamma, q_learning.example_alpha, q_learning.f_1)
+    for i in range(training_games):
+        if (i % 1000 == 0): print("i: "+str(i))
+        agent.do_game()
+    print("finished training!")
+
+    pg = PongGUIHuman()
+    pg.init()
+
     while True:
-        g = game.Game()
-        refresh(g, 100)
+        g = game_human.GameHuman()
+        pg.refresh(g, 50)
         while not g.lost_game():
-            action = random.choice([game.Action.NOTHING,game.Action.UP,game.Action.DOWN])
-            g.do_frame(action)
-            refresh(g, 100)
+            action = agent.get_action(g.get_discrete_state())
+
+            keys = pygame.key.get_pressed()
+            human = game.Action.NOTHING
+            if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+                human = game.Action.UP
+            elif not keys[pygame.K_UP] and keys[pygame.K_DOWN]:
+                human = game.Action.DOWN
+
+            g.do_frame(action, human)
+            pg.refresh(g, 50)
