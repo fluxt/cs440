d4e59e3834f2b1ad55ce3024379dd872c9a9c7c7
HoelzelJon <hoelzeljon@gmail.com>
2018-04-28 01:54:41 -0500

    reflex push

--- mp4/reflex.py
+++ mp4/reflex.py
@@ -5,17 +5,23 @@ def get_action(state):
     x_dist = 1 + ball_x if vel_x < 0 else 1 - ball_x
     y_dist = x_dist * vel_y / abs(vel_x)
     y_goal = (y_dist + ball_y)
+
     
-    if (y_goal > pad_y + game.paddle_height):
+
+    pad_center = pad_y + (game.paddle_height / 2)
+    if (y_goal > pad_center):
         return game.Action.DOWN
-    elif (y_goal < pad_y):
+    elif (y_goal < pad_center):
         return game.Action.UP
     else:
         return game.Action.NOTHING
 
 if __name__ == "__main__":
-    for i in range(20):
+    num_games = 1000
+    sum = 0
+    for i in range(num_games):
         g = game.Game()
         while not g.lost_game():
             g.do_frame(get_action(g.get_state()))
-        print(g.get_num_bounces())
+        sum += g.get_num_bounces()
+    print(sum / num_games)
