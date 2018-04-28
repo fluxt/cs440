2042e4f0a3b610d3544bbda72f68a42dad60c72a
HoelzelJon <hoelzeljon@gmail.com>
2018-04-28 01:43:26 -0500

    updated reflex

--- mp4/reflex.py
+++ mp4/reflex.py
@@ -3,12 +3,19 @@ import game
 def get_action(state):
     ball_x, ball_y, vel_x, vel_y, pad_y = state
     x_dist = 1 + ball_x if vel_x < 0 else 1 - ball_x
-    y_dist = vel_y / abs(vel_x)
+    y_dist = x_dist * vel_y / abs(vel_x)
     y_goal = (y_dist + ball_y)
-    y_goal = y_goal % 1 if y_goal % 2 < 1 else -(y_goal % 1)
+    
     if (y_goal > pad_y + game.paddle_height):
         return game.Action.DOWN
     elif (y_goal < pad_y):
         return game.Action.UP
     else:
         return game.Action.NOTHING
+
+if __name__ == "__main__":
+    for i in range(20):
+        g = game.Game()
+        while not g.lost_game():
+            g.do_frame(get_action(g.get_state()))
+        print(g.get_num_bounces())
