691aba15baa130b20c7ccecca2e2e3c4dd6f11a2
HoelzelJon <hoelzeljon@gmail.com>
2018-04-28 01:32:24 -0500

    fixeed

--- mp4/reflex.py
+++ mp4/reflex.py
@@ -2,7 +2,7 @@ import game
 
 def get_action(state):
     ball_x, ball_y, vel_x, vel_y, pad_y = state
-    x_dist = 1 + ball_x vel_x < 0 else 1 - ball_x
+    x_dist = 1 + ball_x if vel_x < 0 else 1 - ball_x
     y_dist = vel_y / abs(vel_x)
     y_goal = (y_dist + ball_y)
     y_goal = y_goal % 1 if y_goal % 2 < 1 else -(y_goal % 1)
