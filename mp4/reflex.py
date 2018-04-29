9775eab88e7ece082885434c26c94f1128b59a29
HoelzelJon <hoelzeljon@gmail.com>
2018-04-28 21:05:48 -0500

    removed worse reflex agent

--- mp4/reflex.py
+++ mp4/reflex.py
@@ -1,26 +1,6 @@
 import game
 import math
 
-def get_action(state):
-    x_pos, y_pos, vel_x, vel_y, pad_y = state
-    x_dist = 1 + x_pos if vel_x < 0 else 1 - x_pos
-    y_dist = x_dist * vel_y / abs(vel_x)
-    y_goal = (y_dist + y_pos)
-
-    while y_goal < 0 or y_goal > 1:
-        if (y_goal < 0):
-            y_goal *= -1
-        else:
-            y_goal = 2 - y_goal
-
-    pad_center = pad_y + (game.paddle_height / 2)
-    if (y_goal > pad_center):
-        return game.Action.DOWN
-    elif (y_goal < pad_center):
-        return game.Action.UP
-    else:
-        return game.Action.NOTHING
-
 def get_action2(state):
     ball_x, ball_y, vel_x, vel_y, pad_y = state
 
@@ -35,7 +15,7 @@ def get_action2(state):
         elif ball_y > 1:
             ball_y = 2 - ball_y
             vel_y = -vel_y
-        
+
         if ball_x < 0:
             ball_x = -ball_x
             vel_x = -vel_x
